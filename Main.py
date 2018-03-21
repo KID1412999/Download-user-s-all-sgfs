d41a1d7539234b37885e9e126c02e5f067fbbc0f
K.I.D <33932311+KID1412999@users.noreply.github.com>
2018-03-21 22:58:39 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -34,11 +34,19 @@ def get_url():
 		html.encoding="utf-8"
 		ele=etree.HTML(html.text)
 		st=ele.xpath("/html//td/a/@href")
-		print(len(st))
 		t.append(st)
 def product_main_url():
 	for i in range(1,n+1):
 		s.append('http://weiqi.qq.com/qipu/search/title/'+name+'//p/'+str(i)+'.html')
+def check():#检查总页数
+	url0="http://weiqi.qq.com/qipu/search/title/"+name+"//p/1.html"
+	html0=requests.get(url0,headers=head)
+	html0.encoding="utf-8"
+	ele0=etree.HTML(html0.text)
+	str0=ele0.xpath("//ul//li/a/@href")
+	t0=str(str0[-1])
+	t4=re.findall('p/(.+?)\.html',t0)
+	return t4[0]
 def main():
 	product_main_url()
 	get_url()
@@ -48,7 +56,7 @@ def main():
 	exit()
 if "__name__=__main__":
 	name=input("请输入需要下载的棋手名字:")
-	n=int(input("请输入需要下载的页数:"))
+	n=int(input("总共有"+str(check())+"页\n请输入需要下载的页数:"))
 	main()
 
 
