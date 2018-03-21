fdffc610e00d63fd9a5c06b41b6591c2f146d140
K.I.D <33932311+KID1412999@users.noreply.github.com>
2018-03-21 23:02:22 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ s=[]
 t=[]
 q=[]
 n=1
-name="master"
+name="alphaleela"
 head={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36"}
 def deal_url():#构造单个棋谱下载地址
 	for i in range(n):
